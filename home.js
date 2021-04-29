f21452f50c6f925e9f6adc7facd78e03d92bc358
VitalieDragan <80682668+VitalieDragan@users.noreply.github.com>
2021-04-29 15:37:33 +0200

    Add files via upload

--- home.js
+++ home.js
@@ -16,8 +16,22 @@ const searchStates = async searchText => {
         return state.name.match(regex) || state.abbr.match(regex);
     });
 
-    console.log(matches);
+    //console.log(matches);
+    outputHtml(matches);
 };
+//results in HTML
+const outputHtml = matches => {
+    if(matches.length > 0) {
+        const html = matches.map(match => `
+        <div class="card card-body mb-1">
+            <h4>${match.name} (${match.abbr})
+            <span class="text-primary">${match.capital}</span>
+            </h4>
+            <small>Lat: ${match.lat} / Long: ${match.long}</small>
+        </div>
+        `);
+    }
+}
 
  //search.addEventListener('input', searchStates(search.value))
 search.addEventListener('input', () => searchStates(search.value));
